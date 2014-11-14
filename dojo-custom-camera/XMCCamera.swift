1f7a155646cb8f246a423abc6587e54bf5834b38
David McGraw <dlmcgraw07@gmail.com>
2014-11-14 09:45:53 -0600

    project init

---
+++ dojo-custom-camera/XMCCamera.swift
@@ -0,0 +1,167 @@
+//
+//  XMCCamera.swift
+//  dojo-custom-camera
+//
+//  Created by David McGraw on 11/13/14.
+//  Copyright (c) 2014 David McGraw. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+@objc protocol XMCCameraDelegate {
+    func cameraSessionConfigurationDidComplete()
+    func cameraSessionDidBegin()
+    func cameraSessionDidStop()
+}
+
+class XMCCamera: NSObject {
+    
+    weak var delegate: XMCCameraDelegate?
+    
+    var session: AVCaptureSession!
+    var sessionQueue: dispatch_queue_t!
+    var stillImageOutput: AVCaptureStillImageOutput?
+    
+    init(sender: AnyObject) {
+        super.init()
+        self.delegate = sender as? XMCCameraDelegate
+        self.setObservers()
+        self.initializeSession()
+    }
+    
+    deinit {
+        self.removeObservers()
+    }
+    
+    // MARK: Session
+    
+    func initializeSession() {
+        self.session = AVCaptureSession()
+        self.session.sessionPreset = AVCaptureSessionPresetPhoto
+        self.sessionQueue = dispatch_queue_create("camera session", DISPATCH_QUEUE_SERIAL)
+        
+        dispatch_async(self.sessionQueue) {
+            self.session.beginConfiguration()
+            self.addVideoInput()
+            self.addStillImageOutput()
+            self.session.commitConfiguration()
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                NSLog("Session initialization did complete")
+                self.delegate?.cameraSessionConfigurationDidComplete()
+            }
+        }
+    }
+    
+    func startCamera() {
+        dispatch_async(self.sessionQueue) {
+            self.session.startRunning()
+        }
+    }
+    
+    func stopCamera() {
+        dispatch_async(self.sessionQueue) {
+            self.session.stopRunning()
+        }
+    }
+    
+    func captureStillImage(completed: (image: UIImage?) -> Void) {
+        if let imageOutput = self.stillImageOutput {
+            dispatch_async(self.sessionQueue, { () -> Void in
+                
+                var videoConnection: AVCaptureConnection?
+                for connection in imageOutput.connections {
+                    let c = connection as AVCaptureConnection
+                    
+                    for port in c.inputPorts {
+                        let p = port as AVCaptureInputPort
+                        if p.mediaType == AVMediaTypeVideo {
+                            videoConnection = c;
+                            break
+                        }
+                    }
+                    
+                    if videoConnection != nil {
+                        break
+                    }
+                }
+                
+                var error: NSError?
+                imageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: { (imageSampleBuffer: CMSampleBufferRef!, error) -> Void in
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageSampleBuffer)
+                    let image: UIImage? = UIImage(data: imageData!)!
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completed(image: image)
+                    }
+                })
+            })
+        } else {
+            completed(image: nil)
+        }
+    }
+    
+    
+    // MARK: Configuration
+    
+    func addVideoInput() {
+        var error: NSError?
+        var device: AVCaptureDevice = self.deviceWithMediaTypeWithPosition(AVMediaTypeVideo, position: AVCaptureDevicePosition.Back)
+        var input: AVCaptureDeviceInput = AVCaptureDeviceInput.deviceInputWithDevice(device, error: &error) as AVCaptureDeviceInput
+        
+        if error == nil {
+            if self.session.canAddInput(input) {
+                self.session.addInput(input)
+            }
+        }
+    }
+    
+    func addStillImageOutput() {
+        stillImageOutput = AVCaptureStillImageOutput()
+        stillImageOutput?.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        
+        if self.session.canAddOutput(stillImageOutput) {
+            session.addOutput(stillImageOutput)
+        }
+    }
+    
+    func deviceWithMediaTypeWithPosition(mediaType: NSString, position: AVCaptureDevicePosition) -> AVCaptureDevice {
+        var devices: NSArray = AVCaptureDevice.devicesWithMediaType(mediaType)
+        var captureDevice: AVCaptureDevice = devices.firstObject as AVCaptureDevice
+        for device in devices {
+            let d = device as AVCaptureDevice
+            if d.position == position {
+                captureDevice = d
+                break;
+            }
+        }
+        return captureDevice
+    }
+    
+    // MARK: Observers
+    
+    func setObservers() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "sessionDidStart:", name: AVCaptureSessionDidStartRunningNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "sessionDidStop:", name: AVCaptureSessionDidStopRunningNotification, object: nil)
+    }
+    
+    func removeObservers() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func sessionDidStart(notification: NSNotification) {
+        dispatch_async(dispatch_get_main_queue()) {
+            NSLog("Session did start")
+            self.delegate?.cameraSessionDidBegin()
+        }
+    }
+    
+    func sessionDidStop(notification: NSNotification) {
+        dispatch_async(dispatch_get_main_queue()) {
+            NSLog("Session did stop")
+            self.delegate?.cameraSessionDidStop()
+        }
+    }
+    
+}
---
+++ dojo-custom-camera/XMCCameraViewController.swift
@@ -0,0 +1,115 @@
+//
+//  XMCCameraViewController.swift
+//  dojo-custom-camera
+//
+//  Created by David McGraw on 11/13/14.
+//  Copyright (c) 2014 David McGraw. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+enum Status: Int {
+    case Preview, Still, Error
+}
+
+class XMCCameraViewController: UIViewController, XMCCameraDelegate {
+
+    @IBOutlet weak var cameraStill: UIImageView!
+    @IBOutlet weak var cameraPreview: UIView!
+    @IBOutlet weak var cameraStatus: UILabel!
+    @IBOutlet weak var cameraCapture: UIButton!
+    @IBOutlet weak var cameraCaptureShadow: UILabel!
+    
+    var preview: AVCaptureVideoPreviewLayer?
+    
+    var camera: XMCCamera?
+    var status: Status = .Preview
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initializeCamera()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.establishVideoPreviewArea()
+    }
+    
+    func initializeCamera() {
+        self.cameraStatus.text = "Starting Camera"
+        self.camera = XMCCamera(sender: self)
+    }
+    
+    func establishVideoPreviewArea() {
+        self.preview = AVCaptureVideoPreviewLayer(session: self.camera?.session)
+        self.preview?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.preview?.frame = self.cameraPreview.bounds
+        self.preview?.cornerRadius = 8.0
+        self.cameraPreview.layer.addSublayer(self.preview)
+    }
+    
+    // MARK: Button Actions
+    
+    @IBAction func captureFrame(sender: AnyObject) {
+        if self.status == .Preview {
+            self.cameraStatus.text = "Capturing Photo"
+            UIView.animateWithDuration(0.225, animations: { () -> Void in
+                self.cameraPreview.alpha = 0.0;
+                self.cameraStatus.alpha = 1.0
+            })
+            
+            self.camera?.captureStillImage({ (image) -> Void in
+                if image != nil {
+                    self.cameraStill.image = image;
+                
+                    UIView.animateWithDuration(0.225, animations: { () -> Void in
+                        self.cameraStill.alpha = 1.0;
+                        self.cameraStatus.alpha = 0.0;
+                    })
+                    self.status = .Still
+                } else {
+                    self.cameraStatus.text = "Uh oh! Something went wrong. Try it again."
+                    self.status = .Error
+                }
+                
+                self.cameraCapture.setTitle("Reset", forState: UIControlState.Normal)
+            })
+        } else if self.status == .Still || self.status == .Error {
+            UIView.animateWithDuration(0.225, animations: { () -> Void in
+                self.cameraStill.alpha = 0.0;
+                self.cameraStatus.alpha = 0.0;
+                self.cameraPreview.alpha = 1.0;
+                self.cameraCapture.setTitle("Capture", forState: UIControlState.Normal)
+            }, completion: { (done) -> Void in
+                self.cameraStill.image = nil;
+                self.status = .Preview
+            })
+        }
+    }
+    
+    // MARK: Camera Delegate
+    
+    func cameraSessionConfigurationDidComplete() {
+        self.camera?.startCamera()
+    }
+    
+    func cameraSessionDidBegin() {
+        self.cameraStatus.text = ""
+        UIView.animateWithDuration(0.225, animations: { () -> Void in
+            self.cameraStatus.alpha = 0.0
+            self.cameraPreview.alpha = 1.0
+            self.cameraCapture.alpha = 1.0
+            self.cameraCaptureShadow.alpha = 0.4;
+        })
+    }
+    
+    func cameraSessionDidStop() {
+        self.cameraStatus.text = "Camera Stopped"
+        UIView.animateWithDuration(0.225, animations: { () -> Void in
+            self.cameraStatus.alpha = 1.0
+            self.cameraPreview.alpha = 0.0
+        })
+    }
+}
+
